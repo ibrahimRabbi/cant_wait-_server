9c65c4440bfdd1be15d5f444bc5d39ed23a230e6
ibrahim <ibrahimrabbihere@gmail.com>
2025-08-23 13:06:27 +0600

    Removed node_modules and dist from tracking

--- dist/lib/socketCors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.socketCors = void 0;
-exports.socketCors = {
-    cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
-    }
-};
